70aeaf9f77a795be10adf549220cdfc1ad55adf7
Ross Fletcher <rfletcher@outlook.com>
2020-02-07 15:56:13 -0800

    add package structure, icon and set title

--- src/simpletexteditor/SimpleTextEditor.java
+++ src/simpletexteditor/SimpleTextEditor.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -24,6 +25,8 @@ public class SimpleTextEditor extends Application {
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
+        stage.getIcons().add(new Image("/simpletexteditor/resources/pencil.png"));
+        stage.setTitle("Super Simple Editor");
         stage.show();
     }
 
