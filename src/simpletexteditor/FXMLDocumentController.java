f59ad96192c33f5fc10b58682e32d4c7d328cf85
Ross Fletcher <rfletcher@outlook.com>
2020-02-06 18:26:35 -0800

    setup createOpeningFolder method dynamic for windows and mac

--- src/simpletexteditor/FXMLDocumentController.java
+++ src/simpletexteditor/FXMLDocumentController.java
@@ -5,31 +5,58 @@
  */
 package simpletexteditor;
 
+import java.io.File;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javax.swing.JFileChooser;
 
 /**
  *
  * @author rtbfl
  */
 public class FXMLDocumentController implements Initializable {
+    String opSystem = new String();
+    String documentsPath = new JFileChooser().getFileSystemView().getDefaultDirectory().toString();
+    @FXML public TextArea textArea;
     
-    @FXML
-    private Label label;
     
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void createOpeningFolder(){
+        
+        if (opSystem.contains("mac")) {
+            Path path = Paths.get(documentsPath + "/Documents" + "/ssef");
+            if (Files.exists(path)) {
+                    // Do nothing
+            } else {
+            new File(documentsPath + "/Documents" + "/ssef").mkdir();
+            }
+        
+        } else {
+            Path path = Paths.get(documentsPath + "\\ssef");
+            if (Files.exists(path)) {
+                    // Do nothing
+            } else {
+            new File(documentsPath + "\\ssef").mkdir();
+            }
+        }
+        
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        String osName = System.getProperty("os.name").toLowerCase();
+        this.opSystem = osName;
+        System.out.println("Operating System: "   + this.opSystem);
+        textArea.setWrapText(true);
+        createOpeningFolder();
     }    
     
 }
