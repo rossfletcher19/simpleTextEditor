c8774010ec2ec31df067c6b7bca9c5ab89a81bd7
Ross Fletcher <rfletcher@outlook.com>
2020-02-07 15:33:57 -0800

    add save methods and quitApp method

--- src/simpletexteditor/FXMLDocumentController.java
+++ src/simpletexteditor/FXMLDocumentController.java
@@ -6,16 +6,22 @@
 package simpletexteditor;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import javax.swing.JFileChooser;
 
 /**
@@ -23,6 +29,7 @@ import javax.swing.JFileChooser;
  * @author rtbfl
  */
 public class FXMLDocumentController implements Initializable {
+    private Stage primaryStage;
     String opSystem = new String();
     String documentsPath = new JFileChooser().getFileSystemView().getDefaultDirectory().toString();
     @FXML public TextArea textArea;
@@ -47,6 +54,36 @@ public class FXMLDocumentController implements Initializable {
             }
         }
         
+    }
+    
+    @FXML
+    private void saveFile(ActionEvent event){
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(extFilter);
+        File file = fileChooser.showSaveDialog(primaryStage);
+
+        if(file != null){
+
+            Save(textArea.getText().replaceAll("\n", System.getProperty("line.separator")), file);
+        } 
+    }
+    
+    private void Save(String content, File file){
+        try {
+            FileWriter fileWriter;
+            fileWriter = new FileWriter(file);
+            fileWriter.write(content);
+            fileWriter.close();
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLDocumentController.class
+                .getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    @FXML
+    private void quitApp(ActionEvent event){
+        System.exit(0);
     }
     
     @Override
