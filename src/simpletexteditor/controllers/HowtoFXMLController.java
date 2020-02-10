68999419e3053bdfd7df98555ef725937e355c65
Ross Fletcher <rfletcher@outlook.com>
2020-02-09 16:50:51 -0800

    update HowToFXML.fxml with a bit of text and link to README and repo

--- src/simpletexteditor/controllers/HowtoFXMLController.java
+++ src/simpletexteditor/controllers/HowtoFXMLController.java
@@ -4,10 +4,17 @@
  * and open the template in the editor.
  */
 package simpletexteditor.controllers;
-
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
@@ -15,13 +22,23 @@ import javafx.fxml.Initializable;
  * @author rtbfl
  */
 public class HowtoFXMLController implements Initializable {
-
+    Hyperlink link1 = new Hyperlink("Click Me!");
+    
+    @FXML
+    private void linkToReadMe(ActionEvent event) throws IOException, URISyntaxException{
+      
+      if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+    Desktop.getDesktop().browse(new URI("https://github.com/rossfletcher19/simpleTextEditor/blob/master/README.md"));
+        }
+        
+    }
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
     }    
     
 }
